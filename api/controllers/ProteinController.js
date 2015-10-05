ace238e50fdcf2f0963c175e1bf3f2bece35cab9
luluzero <395134629@qq.com>
2015-10-05 18:01:22 +0800

    init commit

---
+++ api/controllers/ProteinController.js
@@ -0,0 +1,12 @@
+/**
+ * ProteinController
+ *
+ * @description :: Server-side logic for managing proteins
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+};
+
---
+++ api/models/Protein.js
@@ -0,0 +1,37 @@
+/**
+* Protein.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes:{
+    id:{
+      type:'string'
+    },
+    infomation:{
+      type:'string'
+    },
+    sequwence:{
+      type:'string'
+    },
+    superclass:{
+      type:'string'
+    },
+    subclass:{
+      type:'string'
+    },
+    state:{
+      type:'string'
+    },
+    info_in_last_db:{
+      type:'string'
+    },
+    dataFileName:{
+      type:'string'
+    }
+  }
+};
+
