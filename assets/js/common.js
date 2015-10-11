48e1339e9b378b0f5c3965c135fa89cf61d2141a
hewenyuan <505553895@qq.com>
2015-10-11 12:17:47 +0800

    公司做的以及在家做的解决冲突的版本

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,7 +5,7 @@ $(function(){
   $(".btn-download").on("click",function(){
     show_modals("myModal2");
   });
-
+  $(".jianjie").css("display","block");
 });
 
 function show_modals(modal_id){
@@ -15,3 +15,10 @@ function show_page(page_class){
   $(".right>div").hide();
   $("."+page_class).show();
 }
+function change_bg_color(obj,str){
+  $(".nav-son").css("background-color","white");
+  $(obj).css("background-color","#337ab7");
+  $(".show-container>div").css("display","none");
+  $("."+str).css("display","block");
+}
+
