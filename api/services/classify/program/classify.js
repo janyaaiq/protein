f3d44b20da7ea761cc02972437e8be5c4a93f1be
luluzero <395134629@qq.com>
2015-10-11 23:50:34 +0800

    alter classify

--- api/services/classify/program/classify.js
+++ api/services/classify/program/classify.js
@@ -2,7 +2,7 @@ var callfile = require('child_process');
 exports.do = function(path,callback){
   var arg1 = path;
   var arg2 = 'rule.example';
-  callfile.exec('perl classify.pl '+arg1+' '+arg2+' ',function (err, stdout, stderr) {
+  callfile.exec('perl /home/allergen/allergen/classify/program/classify.pl '+arg1+' '+arg2+' ',function (err, stdout, stderr) {
     if(err){
       callback(err);
     }else{
